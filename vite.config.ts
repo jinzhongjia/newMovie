a2480565c39c580cde1df98834a82bc3c166db93
jinzhongjia <jinzhongjia@qq.com>
2022-11-10 15:03:49 +0800

    add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import path from "path";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import AutoImport from "unplugin-auto-import/vite";
@@ -6,6 +7,16 @@ import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@views": path.resolve(__dirname, "src/views"),
+      "@public": path.resolve(__dirname, "public"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@composables": path.resolve(__dirname, "src/scomposables"),
+    },
+  },
   plugins: [
     vue(),
     Components({
