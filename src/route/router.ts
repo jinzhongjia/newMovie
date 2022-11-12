fcdd2a18c1977cb4d13f681fe6b89d6b54173f8e
jinzhongjia <jinzhongjia@qq.com>
2022-11-12 17:04:26 +0800

    change hashhistory

--- src/route/router.ts
+++ src/route/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import type { RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
@@ -47,7 +47,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 });
 
