4c4614b7a49fc2eff08e5c5550dd295884cd2369
影翼 <mail@nvimer.org>
2023-06-27 22:17:08 +0800

    小更新，去除某些没必要存在的api路径

--- src/composables/base.ts
+++ src/composables/base.ts
@@ -1,9 +1,9 @@
 let base_url: string;
 
 if (import.meta.env.DEV) {
-  base_url = "http://localhost:8000";
+	base_url = "http://localhost:8000";
 } else {
-  base_url = window.location.protocol + "//" + window.location.host;
+	base_url = window.location.protocol + "//" + window.location.host;
 }
 
 const MainCategory_url = base_url + "/mainCategory";
@@ -12,18 +12,15 @@ const ProxyImg_url = base_url + "/img/proxy?";
 
 const Category_url = base_url + "/category";
 
-const AllCategory_url = base_url + "/allCategory";
-
 const Search_url = base_url + "/search";
 
 const Play_url = base_url + "/play";
 
 export {
-  base_url,
-  MainCategory_url,
-  ProxyImg_url,
-  Category_url,
-  AllCategory_url,
-  Search_url,
-  Play_url,
+	base_url,
+	MainCategory_url,
+	ProxyImg_url,
+	Category_url,
+	Search_url,
+	Play_url,
 };
--- src/composables/store.ts
+++ src/composables/store.ts
@@ -2,12 +2,12 @@ import { defineStore } from "pinia";
 import { computed, Ref, ref } from "vue";
 import { get_json } from "./http";
 import { Category } from "./type";
-import { AllCategory_url } from "./base";
+import {MainCategory_url } from "./base";
 
 export const useCategoryStore = defineStore("counter", () => {
   const categories: Ref<Category[]> = ref([]);
-  get_json(AllCategory_url, {}, (res: Category[]) => {
-    categories.value = res;
+  get_json(MainCategory_url, {}, (res: Category[]) => {
+		categories.value = res;
   });
 
   function Id_to_name(id: number): string {
