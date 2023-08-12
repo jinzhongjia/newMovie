7292b81d117eba406e31a0d0d4a94a8dd83c9210
jinzhongjia <mail@nvimer.org>
2023-08-12 20:45:41 +0800

    dislaimer

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,17 @@ const theme = computed(() => (osThemeRef.value === "dark" ? darkTheme : null));
       <headed />
       <Divider height="10px" />
 
-      <router-view />
+      <div style="margin-bottom: 20px">
+        <router-view />
+      </div>
+      <div class="disclaimer">
+        <div>
+          本站所有视频和图片均来自互联网收集而来，版权归原创者所有，本网站只提供web页面服务，并不提供资源存储，也不参与录制、上传。
+        </div>
+        <div>
+          若本站收录的节目无意侵犯了贵司版权，请发邮件至mail#nvimer.org（#换成@）
+        </div>
+      </div>
     </n-scrollbar>
     <n-global-style />
   </n-config-provider>
@@ -22,5 +32,12 @@ const theme = computed(() => (osThemeRef.value === "dark" ? darkTheme : null));
 .header {
   background-color: gray;
   height: 60px;
+}
+.disclaimer {
+  width: 100%;
+  color: gray;
+  /* position: fixed; */
+  /* bottom: 5px; */
+  text-align: center;
 }
 </style>
